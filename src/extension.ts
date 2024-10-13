4633a1f2d28cc626c8fcdd65853e9fe73e79c058
jyuhou wong <jyuhou.wong@gmail.com>
2024-10-13 12:09:09 +0400

    feat: register extension command

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,70 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { hexoExec, pullHexoRepo, pushToHexoRepo } from "./hexo"; // Import the functions for cloning and pushing
+import { startOAuthLogin } from "./github";
+import { debug } from "console";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Register command for logging into GitHub
+  const loginCommand = vscode.commands.registerCommand(
+    "hexo-github.loginToGitHub",
+    async () => {
+      try {
+        await startOAuthLogin(); // Assuming this function is exported from github.ts
+        vscode.window.showInformationMessage(
+          "Logged into GitHub successfully!"
+        );
+      } catch (error) {
+        vscode.window.showErrorMessage(`Login failed: ${error.message}`);
+      }
+    }
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hexo-github" is now active!');
+  // Register command for cloning the hexo repository
+  const cloneCommand = vscode.commands.registerCommand(
+    "hexo-github.pullHexoRepo",
+    async () => {
+      try {
+        await pullHexoRepo();
+        vscode.window.showInformationMessage(
+          "Hexo repository cloned successfully!"
+        );
+      } catch (error) {
+        vscode.window.showErrorMessage(`Cloning failed: ${error.message}`);
+      }
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('hexo-github.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from hexo-github!');
-	});
+  // Register command for pushing to the hexo repository
+  const pushCommand = vscode.commands.registerCommand(
+    "hexo-github.pushToHexoRepo",
+    async () => {
+      try {
+        await pushToHexoRepo();
+        vscode.window.showInformationMessage(
+          "Pushed to Hexo repository successfully!"
+        );
+      } catch (error) {
+        vscode.window.showErrorMessage(`Pushing failed: ${error.message}`);
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  // Add commands to the context
+  context.subscriptions.push(loginCommand, cloneCommand, pushCommand);
+
+  const server = vscode.commands.registerCommand("hexo-github.server", () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    hexoExec("server", { _: [], debug: true });
+  });
+
+  const newBlog = vscode.commands.registerCommand("hexo-github.new", () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    hexoExec("new", { _: [], path: "test/test" });
+  });
+
+  context.subscriptions.push(server, newBlog);
 }
 
 // This method is called when your extension is deactivated
