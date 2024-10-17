af99b62c4ec6ca3571dcd06d74d20fa6b4409df7
jyuhou wong <jyuhou.wong@gmail.com>
2024-10-18 01:48:11 +0400

    fix:  reveal treeView issue

---
+++ src/providers/index.ts
@@ -0,0 +1,14 @@
+import * as vscode from "vscode";
+import { BlogsTreeDataProvider } from "./blogsTreeDataProvider"; // 引入 BlogsTreeDataProvider
+
+export const registerBlogsProvider = (context: vscode.ExtensionContext) => {
+  // 创建 TreeDataProvider
+  const blogsProvider = new BlogsTreeDataProvider(context);
+  // 创建 TreeView
+  const blogsTreeView = vscode.window.createTreeView("hexo-github-blogs", {
+    treeDataProvider: blogsProvider,
+  });
+  // 注册资源
+  context.subscriptions.push(blogsProvider);
+  context.subscriptions.push(blogsTreeView);
+};
